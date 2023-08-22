bda9fb3a5cee2401e5fed2beb39b25d5ad07835d
Efthakhar <efthakharbin@gmail.com>
2023-08-22 17:34:31 +0600

    Add leave_requests table

---
+++ database/migrations/2023_08_22_112055_create_leave_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leave_requests', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('from');
+            $table->dateTime('to');
+            $table->string('category'); // vacation/sick/leave/personal_problem
+            $table->text('reason');
+            $table->text('status'); // approved/rejected
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leave_requests');
+    }
+};
